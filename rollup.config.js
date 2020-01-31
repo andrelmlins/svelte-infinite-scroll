c06b9655574e9a49a17bf1bc359b17092212ef82
André Lins <andrelucas01@hotmail.com>
2020-01-31 19:08:00 -0300

    Creating a single rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,68 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "rollup-plugin-node-resolve";
 import commonjs from "rollup-plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
+
+const production = !process.env.ROLLUP_WATCH;
 
 export default [
   {
-    input: "src/index.svelte",
+    input: "src/dev/main.js",
+    output: {
+      sourcemap: true,
+      format: "iife",
+      name: "app",
+      file: "public/build/bundle.js"
+    },
+    plugins: [
+      svelte({
+        dev: !production,
+        css: css => {
+          css.write("public/build/bundle.css");
+        }
+      }),
+      resolve({
+        browser: true,
+        dedupe: importee =>
+          importee === "svelte" || importee.startsWith("svelte/")
+      }),
+      commonjs(),
+      !production && serve(),
+      !production && livereload("public"),
+      production && terser()
+    ],
+    watch: {
+      clearScreen: false
+    }
+  },
+  {
+    input: "src/lib/index.svelte",
     output: { file: pkg.main, format: "umd", name: "InfiniteScroll" },
     plugins: [svelte(), resolve(), commonjs()]
   },
   {
-    input: "src/index.svelte",
+    input: "src/lib/index.svelte",
     output: { file: pkg.module, format: "es" },
     external: ["svelte/internal"],
     plugins: [svelte(), commonjs()]
   }
 ];
+
+function serve() {
+  let started = false;
+
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true;
+
+        require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+          stdio: ["ignore", "inherit", "inherit"],
+          shell: true
+        });
+      }
+    }
+  };
+}
