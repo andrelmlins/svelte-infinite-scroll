3f7c2d4e6dc1d3ee1e8a83e728b6ec6fedb1f50c
André Lins <andrelucas01@hotmail.com>
2020-03-28 09:14:14 -0300

    Fix to tests

--- src/lib/Lib.test.js
+++ src/lib/Lib.test.js
@@ -1,6 +1,6 @@
 import { render } from "@testing-library/svelte";
 
-import SvelteInfiniteScroll from "./index.svelte";
+import SvelteInfiniteScroll from "./InfiniteScroll.svelte";
 
 test("shows proper heading when rendered", () => {
   const { container } = render(SvelteInfiniteScroll);
